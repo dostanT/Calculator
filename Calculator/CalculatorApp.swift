f6f0479fdef4b656e6e2c9956b9b1964964fe6cd
Dostan <turlybekdostan@gmail.com>
2025-03-04 19:10:34 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dostan Turlybek on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
