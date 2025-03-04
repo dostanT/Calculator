ab9ca24a7e33107da5ab013c8e4a0368d4a78da3
Dostan <turlybekdostan@gmail.com>
2025-03-04 19:44:41 +0500

    [FUATURE] Create basic logic

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -1,21 +1,68 @@
-//
-//  ContentView.swift
-//  Calculator
-//
-//  Created by Dostan Turlybek on 04.03.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var expression: [String] = []
+    @State private var input: String = ""
+    @State private var result: String = "0"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Text(result)
+                .font(.largeTitle)
+                .padding()
+
+            HStack {
+                Button("1") { addNumber("1") }
+                Button("2") { addNumber("2") }
+            }
+            .buttonStyle(.borderedProminent)
+
+            HStack {
+                Button("+") { addOperator("+") }
+                Button("=") { calculateResult() }
+            }
+            .buttonStyle(.bordered)
+
         }
         .padding()
+    }
+
+    private func addNumber(_ number: String) {
+        input += number
+        result = input
+    }
+
+    private func addOperator(_ op: String) {
+        if !input.isEmpty {
+            expression.append(input)
+            expression.append(op)
+            input = ""
+        }
+    }
+
+    private func calculateResult() {
+        if !input.isEmpty {
+            expression.append(input)
+        }
+
+        var total = 0
+        var currentOperator: String?
+
+        for item in expression {
+            if let number = Int(item) {
+                if let op = currentOperator, op == "+" {
+                    total += number
+                } else {
+                    total = number
+                }
+            } else {
+                currentOperator = item
+            }
+        }
+
+        result = "\(total)"
+        expression.removeAll()
+        input = ""
     }
 }
 
